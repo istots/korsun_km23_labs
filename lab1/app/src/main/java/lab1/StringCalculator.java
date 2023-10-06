e0128ea1b68392f01984bd82c5a5b8adad712e36
Korsun Lisa <eliza.bet.kors@gmail.com>
2023-10-06 20:24:38 +0300

    step 3

--- lab1/app/src/main/java/lab1/StringCalculator.java
+++ lab1/app/src/main/java/lab1/StringCalculator.java
@@ -7,8 +7,12 @@ public class StringCalculator {
 
         if (numbers.length() > 0) {
 
+            if (numbers.endsWith("\\n")) {
+                System.out.printf("Something went wrong! '%s' is invalid.", numbers); 
+                return null;
+            }
             String[] splittedList = null;
-            splittedList = numbers.split(",");
+            splittedList = numbers.split(",|\\\\n");
 
             int accumulator = 0;
             for (String element : splittedList){
@@ -29,11 +33,11 @@ public class StringCalculator {
     public static void main(String[] args) throws Exception {
         System.out.printf("======== LAB 1 ==========\n");
         try (Scanner myscan = new Scanner(System.in)) {
-            System.out.print("\nEnter numbers separated by commas: "); 
+            System.out.print("\nEnter numbers separated by commas or \\n: "); 
             String mystr = myscan.nextLine(); 
             Integer sum = new StringCalculator().add(mystr);
             if (sum != null)
-                System.out.println("Your answer: \n" + sum);
+                System.out.println("Your answer: " + sum);
         } 
         System.out.println("\n=====================\n"); 
     }

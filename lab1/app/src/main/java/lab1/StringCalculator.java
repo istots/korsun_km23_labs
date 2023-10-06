d09079db384a2d9671b7cca31a62acea0e8e6184
Korsun Lisa <eliza.bet.kors@gmail.com>
2023-10-06 20:21:40 +0300

    step 2

--- lab1/app/src/main/java/lab1/StringCalculator.java
+++ lab1/app/src/main/java/lab1/StringCalculator.java
@@ -9,10 +9,6 @@ public class StringCalculator {
 
             String[] splittedList = null;
             splittedList = numbers.split(",");
-            if (splittedList.length > 2) {
-                System.out.printf("More than 2 numbers are not alowed!\n"); 
-                return null;
-            } 
 
             int accumulator = 0;
             for (String element : splittedList){
@@ -33,7 +29,7 @@ public class StringCalculator {
     public static void main(String[] args) throws Exception {
         System.out.printf("======== LAB 1 ==========\n");
         try (Scanner myscan = new Scanner(System.in)) {
-            System.out.print("\nEnter 2 or less numbers separated by comma: "); 
+            System.out.print("\nEnter numbers separated by commas: "); 
             String mystr = myscan.nextLine(); 
             Integer sum = new StringCalculator().add(mystr);
             if (sum != null)
